5127ad5f79b8acab32d76eee6f4ffb3770b9f8cb
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-05-07 17:56:11 +0200

    Initial Commit

---
+++ SmartBreak/SmartBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBreakApp.swift
+//  SmartBreak
+//
+//  Created by Hugo Peyron on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
